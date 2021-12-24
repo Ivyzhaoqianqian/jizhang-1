391fdde31a45462a852cddbf771c0e3de835ccbd
Ivyzhao <1490217104@qq.com>
2021-12-24 21:59:52 +0800

    set types

---
+++ src/components/Types.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <ul class="types">
+      <li :class="type === '-' && 'selected'" @click="selectType('-')">支出</li>
+      <li :class="type === '+' && 'selected'" @click="selectType('+')">收入</li>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Types",
+  data() {
+    return {
+      type: "-",
+    };
+  },
+  methods: {
+    selectType(type: string) {
+      if (type !== "-" && type !== "+") {
+        throw new Error("type is unknown");
+      }
+      this.type = type;
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "~@/assets/styles/helper.scss";
+.types {
+  background: #c4c4c4;
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    &.selected::after {
+      content: "";
+      position: absolute;
+      bottom: 0;
+      left: 0;
+      width: 100%;
+      height: 4px;
+      background: #333;
+    }
+  }
+}
+</style>
